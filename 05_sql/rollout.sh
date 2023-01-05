a3265906306a321b18b04786368eb02aa4b5a7e6
Ryan Wei <rainmanwy@gmail.com>
2023-01-05 17:45:11 +0800

    add switch for analyzedb

--- 05_sql/rollout.sh
+++ 05_sql/rollout.sh
@@ -6,6 +6,48 @@ PWD=$(get_pwd ${BASH_SOURCE[0]})
 step="sql"
 init_log ${step}
 
+if [ "${RUN_ANALYZE}" == "true" ]; then
+
+# max_id=$(ls ${PWD}/*.sql | tail -1)
+# i=$(basename ${max_id} | awk -F '.' '{print $1}' | sed 's/^0*//')
+
+  dbname="$PGDATABASE"
+  if [ "${dbname}" == "" ]; then
+    dbname="${ADMIN_USER}"
+  fi
+
+  if [ "${PGPORT}" == "" ]; then
+    export PGPORT=5432
+  fi
+
+  schema_name=${SCHEMA_NAME}
+  table_name="tpcds"
+
+  start_log
+  #Analyze schema using analyzedb
+  analyzedb -d ${dbname} -s ${SCHEMA_NAME} --full -a
+
+  #make sure root stats are gathered
+  if [ "${VERSION}" == "gpdb_5" ]; then
+    SQL_QUERY="select n.nspname, c.relname from pg_class c join pg_namespace n on c.relnamespace = n.oid join pg_partitions p on p.schemaname = n.nspname and p.tablename = c.relname where n.nspname = 'tpch' and p.partitionrank is null and c.reltuples = 0 order by 1, 2"
+  elif [ "${VERSION}" == "gpdb_6" ]; then
+    SQL_QUERY="select n.nspname, c.relname from pg_class c join pg_namespace n on c.relnamespace = n.oid left outer join (select starelid from pg_statistic group by starelid) s on c.oid = s.starelid join (select tablename from pg_partitions group by tablename) p on p.tablename = c.relname where n.nspname = 'tpch' and s.starelid is null order by 1, 2"
+  else
+    SQL_QUERY="select n.nspname, c.relname from pg_class c join pg_namespace n on c.relnamespace = n.oid left outer join (select starelid from pg_statistic group by starelid) s on c.oid = s.starelid join pg_partitioned_table p on p.partrelid = c.oid where n.nspname = 'tpch' and s.starelid is null order by 1, 2"
+  fi
+  for t in $(psql -v ON_ERROR_STOP=1 -q -t -A -c "${SQL_QUERY}"); do
+    schema_name=$(echo ${t} | awk -F '|' '{print $1}')
+    table_name=$(echo ${t} | awk -F '|' '{print $2}')
+    echo "Missing root stats for ${schema_name}.${table_name}"
+    SQL_QUERY="ANALYZE ROOTPARTITION ${schema_name}.${table_name}"
+    log_time "psql -v ON_ERROR_STOP=1 -q -t -A -c \"${SQL_QUERY}\""
+    psql -v ON_ERROR_STOP=1 -q -t -A -c "${SQL_QUERY}"
+  done
+
+  tuples="0"
+  print_log ${tuples}
+fi
+
 rm -f ${TPC_DS_DIR}/log/*single.explain_analyze.log
 for i in ${PWD}/*.${BENCH_ROLE}.*.sql; do
   for _ in $(seq 1 ${SINGLE_USER_ITERATIONS}); do
