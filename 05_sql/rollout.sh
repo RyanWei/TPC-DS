7f0f1921268c942be98ca8aec61752068f4ccfea
weiyi <weiyi@hashdata.cn>
2023-03-18 11:55:14 +0800

    bugfix for gpdb5

--- 05_sql/rollout.sh
+++ 05_sql/rollout.sh
@@ -28,13 +28,12 @@ if [ "${RUN_ANALYZE}" == "true" ]; then
   analyzedb -d ${dbname} -s ${SCHEMA_NAME} --full -a
 
   #make sure root stats are gathered
-  if [ "${VERSION}" == "gpdb_5" ]; then
-    SQL_QUERY="select n.nspname, c.relname from pg_class c join pg_namespace n on c.relnamespace = n.oid join pg_partitions p on p.schemaname = n.nspname and p.tablename = c.relname where n.nspname = '${SCHEMA_NAME}' and p.partitionrank is null and c.reltuples = 0 order by 1, 2"
-  elif [ "${VERSION}" == "gpdb_6" ]; then
+  if [ "${VERSION}" == "gpdb_5" ] || [ "${VERSION}" == "gpdb_6" ]; then
     SQL_QUERY="select n.nspname, c.relname from pg_class c join pg_namespace n on c.relnamespace = n.oid left outer join (select starelid from pg_statistic group by starelid) s on c.oid = s.starelid join (select tablename from pg_partitions group by tablename) p on p.tablename = c.relname where n.nspname = '${SCHEMA_NAME}' and s.starelid is null order by 1, 2"
   else
     SQL_QUERY="select n.nspname, c.relname from pg_class c join pg_namespace n on c.relnamespace = n.oid left outer join (select starelid from pg_statistic group by starelid) s on c.oid = s.starelid join pg_partitioned_table p on p.partrelid = c.oid where n.nspname = '${SCHEMA_NAME}' and s.starelid is null order by 1, 2"
   fi
+  
   for t in $(psql -v ON_ERROR_STOP=1 -q -t -A -c "${SQL_QUERY}"); do
     schema_name=$(echo ${t} | awk -F '|' '{print $1}')
     table_name=$(echo ${t} | awk -F '|' '{print $2}')
