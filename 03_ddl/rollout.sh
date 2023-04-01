e62f41a34235a40bf105b81f0ea6371e406b5e9d
Ryan Wei <RyanWei@users.noreply.github.com>
2023-04-01 10:12:55 +0800

    Revert "MergePivotalmain"

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
 set -e
 
-PWD=$(get_pwd "${BASH_SOURCE[0]}")
+PWD=$(get_pwd ${BASH_SOURCE[0]})
 
 step="ddl"
-init_log "${step}"
+init_log ${step}
 get_version
 
 filter="gpdb"
 
-#Create tables
-for i in "${PWD}"/*."${filter}".*.sql; do
-  start_log
-  id=$(basename "${i}" | awk -F '.' '{print $1}')
-  schema_name=$(basename "${i}" | awk -F '.' '{print $2}')
-  table_name=$(basename "${i}" | awk -F '.' '{print $3}')
+if [ "${DROP_EXISTING_TABLES}" == "true" ]; then
+  #Create tables
+  for i in ${PWD}/*.${filter}.*.sql; do
+    start_log
+    id=$(echo ${i} | awk -F '.' '{print $1}')
+    export id
+    schema_name=$(echo ${i} | awk -F '.' '{print $2}')
+    export schema_name
+    table_name=$(echo ${i} | awk -F '.' '{print $3}')
+    export table_name
 
-  if [ "${RANDOM_DISTRIBUTION}" == "true" ]; then
-    DISTRIBUTED_BY="DISTRIBUTED RANDOMLY"
-  else
-    while IFS= read -r z; do
-      table_name2=$(echo "${z}" | awk -F '|' '{print $2}')
-      if [ "${table_name2}" == "${table_name}" ]; then
-        distribution=$(echo "${z}" | awk -F '|' '{print $3}')
-      fi
-    done < "${PWD}"/distribution.txt
-    DISTRIBUTED_BY="DISTRIBUTED BY (${distribution})"
-  fi
+    if [ "${RANDOM_DISTRIBUTION}" == "true" ]; then
+      DISTRIBUTED_BY="DISTRIBUTED RANDOMLY"
+    else
+      for z in $(cat ${PWD}/distribution.txt); do
+        table_name2=$(echo ${z} | awk -F '|' '{print $2}')
+        if [ "${table_name2}" == "${table_name}" ]; then
+          distribution=$(echo ${z} | awk -F '|' '{print $3}')
+        fi
+      done
+      DISTRIBUTED_BY="DISTRIBUTED BY (${distribution})"
+    fi
 
-  log_time "psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v SMALL_STORAGE=\"${SMALL_STORAGE}\" -v MEDIUM_STORAGE=\"${MEDIUM_STORAGE}\" -v LARGE_STORAGE=\"${LARGE_STORAGE}\" -v DISTRIBUTED_BY=\"${DISTRIBUTED_BY}\""
-  psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f "${i}" -v SMALL_STORAGE="${SMALL_STORAGE}" -v MEDIUM_STORAGE="${MEDIUM_STORAGE}" -v LARGE_STORAGE="${LARGE_STORAGE}" -v DISTRIBUTED_BY="${DISTRIBUTED_BY}"
+    log_time "psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v SMALL_STORAGE=\"${SMALL_STORAGE}\" -v MEDIUM_STORAGE=\"${MEDIUM_STORAGE}\" -v LARGE_STORAGE=\"${LARGE_STORAGE}\" -v DISTRIBUTED_BY=\"${DISTRIBUTED_BY}\""
+    psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v SMALL_STORAGE="${SMALL_STORAGE}" -v MEDIUM_STORAGE="${MEDIUM_STORAGE}" -v LARGE_STORAGE="${LARGE_STORAGE}" -v DISTRIBUTED_BY="${DISTRIBUTED_BY}"
 
-  print_log "${id}" "${schema_name}" "${table_name}" "0"
-done
+    print_log
+  done
 
-#external tables are the same for all gpdb
-get_gpfdist_port
+  #external tables are the same for all gpdb
+  get_gpfdist_port
 
-for i in "${PWD}"/*.ext_tpcds.*.sql; do
-  start_log
+  for i in ${PWD}/*.ext_tpcds.*.sql; do
+    start_log
 
-  id=$(basename "${i}" | awk -F '.' '{print $1}')
-  schema_name=$(basename "${i}" | awk -F '.' '{print $2}')
-  table_name=$(basename "${i}" | awk -F '.' '{print $3}')
+    id=$(echo ${i} | awk -F '.' '{print $1}')
+    schema_name=$(echo ${i} | awk -F '.' '{print $2}')
+    table_name=$(echo ${i} | awk -F '.' '{print $3}')
 
-  counter=0
+    counter=0
 
     if [ "${VERSION}" == "gpdb_6" ] || [ "${VERSION}" == "gpdb_7" ] || [ "${VERSION}" == "gpdb_8" ]; then
       SQL_QUERY="select rank() over(partition by g.hostname order by g.datadir), g.hostname from gp_segment_configuration g where g.content >= 0 and g.role = '${GPFDIST_LOCATION}' order by g.hostname"
@@ -56,22 +60,23 @@ for i in "${PWD}"/*.ext_tpcds.*.sql; do
       EXT_HOST=$(echo ${x} | awk -F '|' '{print $2}')
       PORT=$((GPFDIST_PORT + CHILD))
 
-    if [ "${counter}" -eq "0" ]; then
-      LOCATION="'"
-    else
-      LOCATION+="', '"
-    fi
-    LOCATION+="gpfdist://${EXT_HOST}:${PORT}/${table_name}_[0-9]*_[0-9]*.dat"
+      if [ "${counter}" -eq "0" ]; then
+        LOCATION="'"
+      else
+        LOCATION+="', '"
+      fi
+      LOCATION+="gpfdist://${EXT_HOST}:${PORT}/${table_name}_[0-9]*_[0-9]*.dat"
 
-    counter=$((counter + 1))
+      counter=$((counter + 1))
+    done
+    LOCATION+="'"
+
+    log_time "psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v LOCATION=\"${LOCATION}\""
+    psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v LOCATION="${LOCATION}"
+
+    print_log
   done
-  LOCATION+="'"
-
-  log_time "psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v LOCATION=\"${LOCATION}\""
-  psql -v ON_ERROR_STOP=1 -q -a -P pager=off -f "${i}" -v LOCATION="${LOCATION}"
-
-  print_log "${id}" "${schema_name}" "${table_name}" "0"
-done
+fi
 
 DropRole="DROP ROLE IF EXISTS ${BENCH_ROLE}"
 CreateRole="CREATE ROLE ${BENCH_ROLE}"
@@ -95,6 +100,6 @@ fi
 log_time "Set search_path for database gpadmin"
 psql -v ON_ERROR_STOP=0 -q -P pager=off -c "${SetSearchPath}"
 
-print_log "${id}" "${schema_name}" "${table_name}" "0"
+print_log
 
 echo "Finished ${step}"
